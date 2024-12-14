1024df12207a51e0ada7d7c90d3ee4adc2a39825
Lokesh K <lokeshkumaravel29@gmail.com>
2024-12-14 23:02:37 +0530

    Product update

--- frontend/src/components/cart/CartItem.js
+++ frontend/src/components/cart/CartItem.js
@@ -1,51 +1,54 @@
-import React, { useContext } from 'react';
-import { TbTrash } from 'react-icons/tb';
-import { Link } from 'react-router-dom';
-import { displayMoney } from '../../helpers/utils';
-import cartContext from '../../contexts/cart/cartContext';
-import QuantityBox from '../common/QuantityBox';
+import React, { useContext } from "react";
+import { TbTrash } from "react-icons/tb";
+import { Link } from "react-router-dom";
+import { displayMoney } from "../../helpers/utils";
+import cartContext from "../../contexts/cart/cartContext";
+import QuantityBox from "../common/QuantityBox";
 
 const CartItem = (props) => {
-    const { _id, images, title, originalPrice, finalPrice, quantity, path } = props;
-    const { removeItem, updateItemQuantity } = useContext(cartContext);
-  
-    const newPrice = displayMoney(finalPrice);
-    const oldPrice = displayMoney(originalPrice);
-    console.log(_id);
-    
-    return (
-        <div className="cart_item">
-            <figure className="cart_item_img">
-                <Link to={`${path}${_id}`}>
-                    <img src={images} alt="product-img" />
-                </Link>
-            </figure>
-            <div className="cart_item_info">
-                <div className="cart_item_head">
-                    <h4 className="cart_item_title">
-                        <Link to={`/product-details/${_id}`}>{title}</Link>
-                    </h4>
-                    <div className="cart_item_del">
-                        <span onClick={() => removeItem(_id)}>
-                            <TbTrash />
-                        </span>
-                        <div className="tooltip">Remove Item</div>
-                    </div>
-                </div>
+  const { _id, images, title, originalPrice, finalPrice, quantity, path } =
+    props;
+  const { removeItem, updateItemQuantity } = useContext(cartContext);
 
-                <h2 className="cart_item_price">
-                    {newPrice} &nbsp;
-                    <small><del>{oldPrice}</del></small>
-                </h2>
+  const newPrice = displayMoney(finalPrice);
+  const oldPrice = displayMoney(originalPrice);
+  // console.log(_id);
 
-                <QuantityBox 
-                    itemId={_id} 
-                    itemQuantity={quantity} 
-                    updateQuantity={updateItemQuantity} 
-                />
-            </div>
+  return (
+    <div className="cart_item">
+      <figure className="cart_item_img">
+        <Link to={`${path}${_id}`}>
+          <img src={images} alt="product-img" />
+        </Link>
+      </figure>
+      <div className="cart_item_info">
+        <div className="cart_item_head">
+          <h4 className="cart_item_title">
+            <Link to={`/product-details/${_id}`}>{title}</Link>
+          </h4>
+          <div className="cart_item_del">
+            <span onClick={() => removeItem(_id)}>
+              <TbTrash />
+            </span>
+            <div className="tooltip">Remove Item</div>
+          </div>
         </div>
-    );
+
+        <h2 className="cart_item_price">
+          {newPrice} &nbsp;
+          <small>
+            <del>{oldPrice}</del>
+          </small>
+        </h2>
+
+        <QuantityBox
+          itemId={_id}
+          itemQuantity={quantity}
+          updateQuantity={updateItemQuantity}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -1,98 +1,107 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { AiOutlineSearch, AiOutlineShoppingCart, AiOutlineUser } from 'react-icons/ai';
-import { dropdownMenu } from '../../data/headerData';
-import commonContext from '../../contexts/common/commonContext';
-import cartContext from '../../contexts/cart/cartContext';
-import AccountForm from '../form/AccountForm';
-import SearchBar from './SearchBar';
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  AiOutlineSearch,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+} from "react-icons/ai";
+import { dropdownMenu } from "../../data/headerData";
+import commonContext from "../../contexts/common/commonContext";
+import cartContext from "../../contexts/cart/cartContext";
+import AccountForm from "../form/AccountForm";
+import SearchBar from "./SearchBar";
 
 const Header = () => {
-    const { formUserInfo, toggleForm, toggleSearch, setFormUserInfo } = useContext(commonContext);
-    const { cartItems } = useContext(cartContext);
-    const [isSticky, setIsSticky] = useState(false);
+  const { formUserInfo, toggleForm, toggleSearch, setFormUserInfo } =
+    useContext(commonContext);
+  const { cartItems } = useContext(cartContext);
+  const [isSticky, setIsSticky] = useState(false);
 
-    useEffect(() => {
-        const storedUser = localStorage.getItem('user');
-        if (storedUser) {
-            const userData = JSON.parse(storedUser);
-            setFormUserInfo(userData.user.username); 
-        }
-    }, []);
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      const userData = JSON.parse(storedUser);
+      localStorage.setItem("userId", userData.user._id);
+      setFormUserInfo(userData.user.username);
+    }
+  }, []);
 
-    useEffect(() => {
-        const handleIsSticky = () => window.scrollY >= 50 ? setIsSticky(true) : setIsSticky(false);
-        window.addEventListener('scroll', handleIsSticky);
-        return () => {
-            window.removeEventListener('scroll', handleIsSticky);
-        };
-    }, []);
+  useEffect(() => {
+    const handleIsSticky = () =>
+      window.scrollY >= 50 ? setIsSticky(true) : setIsSticky(false);
+    window.addEventListener("scroll", handleIsSticky);
+    return () => {
+      window.removeEventListener("scroll", handleIsSticky);
+    };
+  }, []);
 
-    const cartQuantity = cartItems.length;
+  const cartQuantity = cartItems.length;
 
-    return (
-        <>
-            <header id="header" className={isSticky ? 'sticky' : ''}>
-                <div className="container">
-                    <div className="navbar">
-                        <h2 className="nav_logo">
-                            <Link to="/">ZOROZ</Link>
-                        </h2>
-                        <nav className="nav_actions">
-                            <div className="search_action">
-                                <span onClick={() => toggleSearch(true)}>
-                                    <AiOutlineSearch />
-                                </span>
-                                <div className="tooltip">Search</div>
-                            </div>
+  return (
+    <>
+      <header id="header" className={isSticky ? "sticky" : ""}>
+        <div className="container">
+          <div className="navbar">
+            <h2 className="nav_logo">
+              <Link to="/">ZOROZ</Link>
+            </h2>
+            <nav className="nav_actions">
+              <div className="search_action">
+                <span onClick={() => toggleSearch(true)}>
+                  <AiOutlineSearch />
+                </span>
+                <div className="tooltip">Search</div>
+              </div>
 
-                            <div className="cart_action">
-                                <Link to="/cart">
-                                    <AiOutlineShoppingCart />
-                                    {cartQuantity > 0 && (
-                                        <span className="badge">{cartQuantity}</span>
-                                    )}
-                                </Link>
-                                <div className="tooltip">Cart</div>
-                            </div>
+              <div className="cart_action">
+                <Link to="/cart">
+                  <AiOutlineShoppingCart />
+                  {cartQuantity > 0 && (
+                    <span className="badge">{cartQuantity}</span>
+                  )}
+                </Link>
+                <div className="tooltip">Cart</div>
+              </div>
 
-                            <div className="user_action">
-                                <span>
-                                    <AiOutlineUser />
-                                </span>
-                                <div className="dropdown_menu">
-                                    <h4>Hello! {formUserInfo && <Link to="/profile">&nbsp;{formUserInfo}</Link>}</h4>
-                                    <p>Access account and manage orders</p>
-                                    {!formUserInfo && (
-                                        <button
-                                            type="button"
-                                            onClick={() => toggleForm(true)}
-                                        >
-                                            Login / Signup
-                                        </button>
-                                    )}
-                                    <div className="separator"></div>
-                                    <ul>
-                                        {dropdownMenu.map(item => {
-                                            const { _id, link, path } = item;
-                                            return (
-                                                <li key={_id}>
-                                                    <Link to={path}>{link}</Link>
-                                                </li>
-                                            );
-                                        })}
-                                    </ul>
-                                </div>
-                            </div>
-                        </nav>
-                    </div>
+              <div className="user_action">
+                <span>
+                  <AiOutlineUser />
+                </span>
+                <div className="dropdown_menu">
+                  <h4>
+                    Hello!{" "}
+                    {formUserInfo && (
+                      <Link to="/profile">&nbsp;{formUserInfo}</Link>
+                    )}
+                  </h4>
+                  <p>Access account and manage orders</p>
+                  {!formUserInfo && (
+                    <button type="button" onClick={() => toggleForm(true)}>
+                      Login / Signup
+                    </button>
+                  )}
+                  <div className="separator"></div>
+                  <ul>
+                    {dropdownMenu.map((item) => {
+                      const { _id, link, path } = item;
+                      return (
+                        <li key={_id}>
+                          <Link to={path}>{link}</Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
                 </div>
-            </header>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </header>
 
-            <SearchBar />
-            <AccountForm />
-        </>
-    );
+      <SearchBar />
+      <AccountForm />
+    </>
+  );
 };
 
 export default Header;
