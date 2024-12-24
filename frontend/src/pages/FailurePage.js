1352689b3bc0554238b9e8bd59fb611f771f256d
Lokesh K <lokeshkumaravel29@gmail.com>
2024-12-24 23:10:35 +0530

    twits

--- frontend/src/pages/FailurePage.js
+++ frontend/src/pages/FailurePage.js
@@ -8,9 +8,6 @@ const FailurePage = () => {
         <div className="container">
           <div className="error_page_content">
             <h2 style={{ color: "red" }}>Payment Failure !</h2>
-            {/* <h3 style={{ color: "white" }}>
-              You will receive the order status later{" "}
-            </h3> */}
             <h3 style={{ color: "white" }}>
               Please check your mail after few minutes
             </h3>
